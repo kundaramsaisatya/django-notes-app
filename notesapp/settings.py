14c041610752a0d513f1257fc3fd4c388ba0a8c2
Saisatya <113768956+kundaramsaisatya@users.noreply.github.com>
2025-08-01 18:32:27 +0530

    Update settings.py

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST","db"),
-        'PORT': os.getenv("DB_PORT","3306"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
